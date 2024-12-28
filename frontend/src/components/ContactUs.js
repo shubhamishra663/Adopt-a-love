6d1d22af2ba96307d01a5e77ce1a18833655e98b
shubhamishra663 <126072210+shubhamishra663@users.noreply.github.com>
2024-12-28 13:53:56 +0530

    ContactUs update

--- frontend/src/components/ContactUs.js
+++ frontend/src/components/ContactUs.js
@@ -1,6 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ContactUs = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    mobileNo: "",
+    subject: "",
+    message: "",
+    status: false,
+  });
+
+  const submission = async () => {
+    try {
+      const res = await fetch("https://adopt-a-love-backend.vercel.app/contact-us", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (res.ok) {
+        const responseData = await res.json();
+        alert("Success", "We will contact you soon."); // Use window.alert() for web
+        setFormData({
+          name: "",
+          email: "",
+          mobileNo: "",
+          subject: "",
+          message: "",
+          status: false,
+        });
+        setIsSubmitting(false);
+      } else {
+        console.log(`Request failed with status: ${res.status}`);
+        alert("Error", "Failed to contact.");
+        setIsSubmitting(false);
+      }
+    } catch (error) {
+      console.log("Error submitting contact form:", error);
+      alert("Error", "An error occurred while submitting contact details.");
+      setIsSubmitting(false);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!formData.name || !formData.email || !formData.message) {
+      alert("Error", "Please fill in all required fields.");
+      return;
+    }
+    setIsSubmitting(true);
+    submission();
+  };
+
   return (
     <div className="bg-gray-100 dark:bg-black min-h-screen flex flex-col items-center py-4">
       <div className="text-center">
@@ -11,6 +74,7 @@ const ContactUs = () => {
         </p>
       </div>
       <div className="flex flex-wrap justify-center gap-8 mb-20 py-5">
+        {/* Contact Information */}
         <div className="flex flex-col items-center">
           <div className="bg-blue-100 p-4 rounded-full mb-4">
             <span role="img" aria-label="Call us">
@@ -41,12 +105,15 @@ const ContactUs = () => {
           <p className="text-gray-600 dark:text-white">Sarari, Danapur, Patna 801105</p>
         </div>
       </div>
-      <form className="bg-white p-8 rounded-lg shadow-lg max-w-3xl w-full dark:bg-gray-950">
+      <form className="bg-white p-8 rounded-lg shadow-lg max-w-3xl w-full dark:bg-gray-950" onSubmit={handleSubmit}>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           <div>
             <label className="block text-gray-700 mb-2 dark:text-gray-400">First & Last Name</label>
             <input
+              name="name"
+              onChange={handleChange}
               type="text"
+              value={formData.name}
               placeholder="i.e. John Doe"
               className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
@@ -54,7 +121,10 @@ const ContactUs = () => {
           <div>
             <label className="block text-gray-700 mb-2 dark:text-gray-400">Email</label>
             <input
+              name="email"
+              onChange={handleChange}
               type="email"
+              value={formData.email}
               placeholder="i.e. john@mail.com"
               className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
@@ -62,7 +132,10 @@ const ContactUs = () => {
           <div>
             <label className="block text-gray-700 mb-2 dark:text-gray-400">Phone Number</label>
             <input
+              name="mobileNo"
+              onChange={handleChange}
               type="tel"
+              value={formData.mobileNo}
               placeholder="i.e. +1-234-567-7890"
               className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
@@ -70,7 +143,10 @@ const ContactUs = () => {
           <div>
             <label className="block text-gray-700 mb-2 dark:text-gray-400">Subject</label>
             <input
+              name="subject"
+              onChange={handleChange}
               type="text"
+              value={formData.subject}
               placeholder="i.e. I need help"
               className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
@@ -79,6 +155,9 @@ const ContactUs = () => {
         <div className="mt-6">
           <label className="block text-gray-700 mb-2 dark:text-gray-400">Message</label>
           <textarea
+            name="message"
+            onChange={handleChange}
+            value={formData.message}
             placeholder="Type your message"
             className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
             rows="4"
@@ -87,8 +166,9 @@ const ContactUs = () => {
         <button
           type="submit"
           className="mt-6 bg-blue-500 text-white font-bold py-2 px-6 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          disabled={isSubmitting}
         >
-          Send
+          {isSubmitting ? "Submitting..." : "Send"}
         </button>
       </form>
     </div>
